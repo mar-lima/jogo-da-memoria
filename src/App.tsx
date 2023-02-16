26014b44ef772a45f082dcda334e38a6438a455e
Marcelo Lima <marcelo.llima135@gmail.com>
2023-02-15 23:29:35 -0300

    Inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as C from "./App.styles";
+import logoImage from "./assets/devmemory_logo.png";
+import InfoItem from "./components/InfoItem";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+        <C.InfoArea>
+          <InfoItem label="Tempo" value="00:00" />
+          <InfoItem label="Movimento" value="0" />
+        </C.InfoArea>
+        <button>Reiniciar</button>
+      </C.Info>
+      <C.GridArea>...</C.GridArea>
+    </C.Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/InfoItem/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 20px 0;
+`;
+
+export const Label = styled.div`
+  font-size: 15px;
+  color: #6a7d8b;
+`;
+
+export const Value = styled.div`
+  font-size: 30px;
+`;
