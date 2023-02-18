ae184270f3d61108673858ec0bfcccff29395659
Marcelo Lima <marcelo.llima135@gmail.com>
2023-02-17 23:05:21 -0300

    add cont

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as C from "./App.styles";
 import logoImage from "./assets/devmemory_logo.png";
+import { Button } from "./components/Button";
 import InfoItem from "./components/InfoItem";
+import RestartIcon from "./svgs/restart.svg";
+import { GridItemType } from "./types/GridItemType";
+import { items } from "./data/items";
+import GridItem from "./components/GridItem";
+import { formarTimeElapsed } from "./helpers/formarTimeElapsed";
 
 const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [showCount, setShowCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => resetAndCreatGrid(), []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (playing) setTimeElapsed(timeElapsed + 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [playing, timeElapsed]);
+
+  const resetAndCreatGrid = () => {
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShowCount(0);
+    let tmpGrid: GridItemType[] = [];
+    for (let i = 0; i < items.length * 2; i++)
+      tmpGrid.push({
+        item: null,
+        shown: false,
+        permanentShown: false,
+      });
+
+    for (let w = 0; w < 2; w++) {
+      for (let i = 0; i < items.length; i++) {
+        let pos = -1;
+        while (pos < 0 || tmpGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+
+        tmpGrid[pos].item = i;
+      }
+    }
+
+    setGridItems(tmpGrid);
+
+    setPlaying(true);
+  };
+
+  const handleItemClick = (index: number) => {};
+
   return (
     <C.Container>
       <C.Info>
@@ -11,12 +62,28 @@ const App = () => {
           <img src={logoImage} width="200" alt="" />
         </C.LogoLink>
         <C.InfoArea>
-          <InfoItem label="Tempo" value="00:00" />
+          {timeElapsed}
+          <InfoItem label="Tempo" value={formarTimeElapsed(timeElapsed)} />
           <InfoItem label="Movimento" value="0" />
         </C.InfoArea>
-        <button>Reiniciar</button>
+
+        <Button
+          label="Reiniciar"
+          icon={RestartIcon}
+          onClick={resetAndCreatGrid}
+        />
       </C.Info>
-      <C.GridArea>...</C.GridArea>
+      <C.GridArea>
+        <C.Grid>
+          {gridItems.map((item, index) => (
+            <GridItem
+              key={index}
+              item={item}
+              onClick={() => handleItemClick(index)}
+            />
+          ))}
+        </C.Grid>
+      </C.GridArea>
     </C.Container>
   );
 };
