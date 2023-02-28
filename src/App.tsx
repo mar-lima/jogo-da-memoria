5b42fe85d6c8e0fcdc43a61dc918f8efe5086b19
Marcelo Lima <marcelo.llima135@gmail.com>
2023-02-27 23:03:55 -0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,47 @@ const App = () => {
     return () => clearInterval(timer);
   }, [playing, timeElapsed]);
 
+  //verify if opened equal
+  useEffect(() => {
+    if (showCount === 2) {
+      let opened = gridItems.filter((item) => item.shown === true);
+      if (opened.length === 2) {
+        let tmpGrid = [...gridItems];
+        if (opened[0].item === opened[1].item) {
+          //v1 - if both are equal, make every "shown" permanet
+          let tmpGrid = [...gridItems];
+          for (let i in tmpGrid) {
+            if (tmpGrid[i].shown) {
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+          setGridItems(tmpGrid);
+          setShowCount(0);
+        } else {
+          //v2 - if they are NOT equal, close all "shown"
+          setTimeout(() => {
+            let tmpGrid = [...gridItems];
+            for (let i in tmpGrid) {
+              tmpGrid[i].shown = false;
+            }
+            setGridItems(tmpGrid);
+            setShowCount(0);
+          }, 1000);
+        }
+
+        setMoveCount((moveCount) => moveCount + 1);
+      }
+    }
+  }, [showCount, gridItems]);
+// verify is game over
+  useEffect(()=>{
+    if(moveCount > 0 && gridItems.every(item => item.permanentShown === true)) {
+      setPlaying(false)
+    }    
+
+  }, [moveCount, gridItems])
+
   const resetAndCreatGrid = () => {
     setTimeElapsed(0);
     setMoveCount(0);
@@ -53,7 +94,20 @@ const App = () => {
     setPlaying(true);
   };
 
-  const handleItemClick = (index: number) => {};
+  const handleItemClick = (index: number) => {
+    if (playing && index !== null && showCount < 2) {
+      let tmpGrid = [...gridItems];
+
+      if (
+        tmpGrid[index].permanentShown === false &&
+        tmpGrid[index].shown === false
+      ) {
+        tmpGrid[index].shown = true;
+        setShowCount(showCount + 1);
+      }
+      setGridItems(tmpGrid);
+    }
+  };
 
   return (
     <C.Container>
@@ -62,9 +116,8 @@ const App = () => {
           <img src={logoImage} width="200" alt="" />
         </C.LogoLink>
         <C.InfoArea>
-          {timeElapsed}
           <InfoItem label="Tempo" value={formarTimeElapsed(timeElapsed)} />
-          <InfoItem label="Movimento" value="0" />
+          <InfoItem label="Movimento" value={`${moveCount}`} />
         </C.InfoArea>
 
         <Button
